e916134fbf72d41fd63a268e7515751aac877199
Pedro Junior <40725275+Pedro-jds@users.noreply.github.com>
2021-11-27 01:59:31 -0300

    swagger adcionado

--- src/app.js
+++ src/app.js
@@ -4,14 +4,23 @@ const app = express()
 const cors = require('cors')
 
 
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+const  options  =  { 
+  customCss : '.swagger-ui .topbar {display: none}' } ;
+
+
 //Middlewares
 app.use(express.json());
 app.use(cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
 
 tarefa(app)
 
 app.get('/', (req, res) => {
-  res.send('Bem vindo ao gerenciador de faturas API')
+  res.send(`<h1>API gerenciador de tarefas</h1>`)
 })
 
 
