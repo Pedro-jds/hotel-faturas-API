45c7a2a11be66bfd74f0f8ae8bedf4ee92ff9b79
Pedro Junior <40725275+Pedro-jds@users.noreply.github.com>
2021-12-03 20:51:16 -0300

    model alterada

--- src/models/faturasModel.js
+++ src/models/faturasModel.js
@@ -1,37 +1,4 @@
 class Faturas {
-  static verificaStatus(statusPagamento) {
-    switch (statusPagamento.toLowerCase()) {
-      case 'pago':
-        break;
-      case 'pendente':
-        break;
-      case 'cancelado':
-        break;
-      default:
-        throw new Error("Status desconhecido, informe entre (pago, pendente ou cancelado)")
-    }
-    return statusPagamento;
-  }
-  static verificaMetodo(metodoPagamento) {
-    switch (metodoPagamento.toLowerCase()) {
-      case 'pix':
-        break;
-      case 'cartao de credito':
-        break;
-      case 'cartao de debito':
-        break;
-      case 'dinheiro':
-        break;
-      case 'cheque':
-        break;
-      case 'boleto':
-        break;
-      default:
-        throw new Error("Status desconhecido, informe entre (pix, cartao de credito, cartao de debito, dinheiro, cheque ou boleto)")
-    }
-    
-    return metodoPagamento;
-  }
   static verificaValorTotal(valorTotal) {
     if (isNaN(valorTotal)||valorTotal > 100000) {
       throw new Error("Digite um valor valido")
@@ -41,7 +8,6 @@ class Faturas {
       return valorTotal;
     }
   }
-
 
   constructor(metodoPagamento, statusPagamento, valorTotal) {
     this.metodoPagamento = Faturas.verificaMetodo(metodoPagamento).toLowerCase()
